31a802648c4e5bc38ada6a247f10d0202ba34610
Matthew Walters <matt@webflame.io>
2025-11-02 01:11:01 -0800

    WIP

--- cmd/root.go
+++ cmd/root.go
@@ -6,6 +6,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 var rootCmd = &cobra.Command{
 	Use:   "drift",
 	Short: "Drift is a tool for detecting and preventing drift between your code and your documentation.",
@@ -14,6 +20,16 @@ var rootCmd = &cobra.Command{
 	},
 }
 
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints the version, commit, and build date",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("drift version %s, commit %s, built at %s
+", version, commit, date)
+	},
+}
+
 func Execute() error {
+	rootCmd.AddCommand(versionCmd)
 	return rootCmd.Execute()
 }
