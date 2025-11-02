7478f4e4f236009348ce43481e2f94947ad47467
Matt Walters <matthewrobertwalters@gmail.com>
2025-11-01 23:01:00 -0700

    Init (#3)
    * WIP
    
    * Test
    
    * WIP

--- cmd/init.go
+++ cmd/init.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/driftee-ai/drift/pkg/config"
 	"github.com/spf13/cobra"
 )
 
@@ -10,7 +12,11 @@ var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initializes a new .drift.yaml configuration file.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("drift init called")
+		err := config.CreateScaffold(".drift.yaml")
+		if err != nil {
+			log.Fatalf("Failed to create .drift.yaml: %v", err)
+		}
+		fmt.Println(".drift.yaml created successfully.")
 	},
 }
 
