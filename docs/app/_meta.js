9b307f889cab8ccafa6ba4200b64c586584e0b85
Matthew Walters <matt@webflame.io>
2025-11-05 20:29:12 -0800

    WIP

--- docs/app/_meta.js
+++
@@ -1,9 +0,0 @@
-export default {
-  index: "My Homepage",
-  // You can use JSX elements to change the look of titles in the sidebar, e.g. insert icons
-  about: {
-    // Alternatively, you can set title with `title` property
-    title: "About Us Foo",
-    // ... and provide extra configurations
-  },
-};
--- docs/content/_meta.js
+++ docs/content/_meta.js
@@ -1,4 +1,9 @@
 export default {
+  index: {
+    // Alternatively, you can set title with `title` property
+    title: "My index",
+    // ... and provide extra configurations
+  },
   about: {
     // Alternatively, you can set title with `title` property
     title: "About Us",
