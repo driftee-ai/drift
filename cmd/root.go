7b5c8e3525ce0d16271b749e523ce098cc8c20b1
Matthew Walters <matt@webflame.io>
2025-11-11 11:25:27 -0800

    Feat: Display usage when no command is provided

--- cmd/root.go
+++ cmd/root.go
@@ -12,21 +12,10 @@ var (
 	date    = "unknown"
 )
 
-func init() {
-	rootCmd.PersistentFlags().BoolP("version", "v", false, "Print the version and exit")
-}
-
 var rootCmd = &cobra.Command{
 	Use:   "drift",
 	Short: "Drift is a tool for detecting and preventing drift between your code and your documentation.",
 	Version: fmt.Sprintf("drift version %s, commit %s, built at %s", version, commit, date),
-	Run: func(cmd *cobra.Command, args []string) {
-		if v, _ := cmd.Flags().GetBool("version"); v {
-			fmt.Println(cmd.Version)
-			return
-		}
-		fmt.Println("Hello from drift!")
-	},
 }
 
 func Execute() error {
